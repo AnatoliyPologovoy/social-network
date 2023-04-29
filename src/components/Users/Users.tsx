ba9cf9da072d557b5f57e3e619d8dc4e8a789f66
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-04-29 06:41:10 +0300

    use method componentDidMount()

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,43 +6,42 @@ import axios from "axios";
 export type UsersPropsType = {
     users: UserStateType[]
     toggleFollow: (userId: number) => void
-    setUsers: (users : UserStateType[]) => void
+    setUsers: (users: UserStateType[]) => void
 }
 
 export class Users extends React.Component<UsersPropsType, any> {
-    constructor(props: UsersPropsType) {
-        super(props);
+
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => this.props.setUsers(response.data.items))
-        // response => this.props.setUsers(response.data.items)
     }
 
     onClickButtonHandler = (id: number) => {
         this.props.toggleFollow(id)
     }
-    render () {
+
+    render() {
         const usersRender = this.props.users.map(us => {
+            const buttonName = us.followed ? 'unFollow' : 'Follow'
+            const urlPhoto = us.photos?.small || "https://i.pravatar.cc/38"
 
-        const buttonName = us.followed ? 'unFollow' : 'Follow'
-        const urlPhoto = us.photos?.small || "https://i.pravatar.cc/38"
-
+            return (
+                <li key={us.id}>
+                    <img alt={'avatar ' + us.name} src={urlPhoto}/>
+                    <button onClick={() => this.onClickButtonHandler(us.id)}>{buttonName}</button>
+                    <div>
+                        <span>{us.name}</span>
+                        <span>{us.status}</span>
+                        {/*<span>{us.location.cityName} + ' ' + {us.location.country} </span>*/}
+                    </div>
+                </li>
+            )
+        })
         return (
-            <li key={us.id}>
-                <img alt={'avatar ' + us.name} src={urlPhoto}/>
-                <button onClick={() => this.onClickButtonHandler(us.id)}>{buttonName}</button>
-                <div>
-                    <span>{us.name}</span>
-                    <span>{us.status}</span>
-                    {/*<span>{us.location.cityName} + ' ' + {us.location.country} </span>*/}
-                </div>
-            </li>
-        )
-    })
-    return (
-        <ul>
-            {usersRender}
-        </ul>
-    );
+            <ul>
+                {usersRender}
+            </ul>
+        );
     }
 }
 
