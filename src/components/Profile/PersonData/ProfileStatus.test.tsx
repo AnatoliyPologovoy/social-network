762a6a80a0657f9c51d53af72718f2968d3f4095
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-07-25 21:05:00 +0300

    fix: add new test for profile-status component

--- src/components/Profile/PersonData/ProfileStatus.test.tsx
+++ src/components/Profile/PersonData/ProfileStatus.test.tsx
@@ -1,11 +1,37 @@
 import {create} from "react-test-renderer";
 import {ProfileStatus} from "components/Profile/PersonData/ProfileStatus";
 
-describe('test1', () => {
-    test('test1-1', () => {
+describe('test profileComponent', () => {
+    test('input should to be status props text', () => {
         const component = create(<ProfileStatus status={'testStatus'}/>)
         const instance = component.getInstance()
         // @ts-ignore
-        expect(instance?.state.status).toBe('testStatus')
+        expect(instance?.state.input).toBe('testStatus')
+    })
+
+    test('span should to be has status props text', () => {
+        const component = create(<ProfileStatus status={'testStatus'}/>)
+        const root = component.root
+        // @ts-ignore
+        let span = root.findByType('span')
+        expect(span?.children.length).toBe(1)
+        expect(span?.children[0]).toBe('testStatus')
+    })
+
+    test('if isEditMode === false input should not be', () => {
+        const component = create(<ProfileStatus status={'testStatus'}/>)
+        const root = component.root
+        //expect error because input undefined
+        expect( () => root.findByType('input')).toThrow()
+    })
+
+    test('if onClick span isEditMode should be true', () => {
+        const component = create(<ProfileStatus status={'testStatus'}/>)
+        const instance = component.getInstance()
+        const root = component.root
+        let span = root.findByType('span')
+        span.props.onDoubleClick()
+        // @ts-ignore
+        expect( instance?.state.isEditMode).toBeTruthy()
     })
 })
