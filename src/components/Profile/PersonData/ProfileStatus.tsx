690e0db3fb5d5305f940f56b8d149005e7e41108
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-07-31 21:11:06 +0300

    fix: styiling status icon-button

--- src/components/Profile/PersonData/ProfileStatus.tsx
+++ src/components/Profile/PersonData/ProfileStatus.tsx
@@ -1,68 +1,75 @@
 import React, {ChangeEvent} from "react";
 import cl from "./personData.module.css"
+import {ReactComponent as EditIcon} from "assets/edit-btn.svg"
+import {ReactComponent as SaveIcon} from "assets/ok-btn.svg"
 
 type StatusPropsType = {
-    status: string
-    updateStatus: (status: string) => void
-    isHostUser: boolean
+		status: string
+		updateStatus: (status: string) => void
+		isHostUser: boolean
 }
 
 export class ProfileStatus extends React.Component<StatusPropsType, any> {
-    state = {
-        isEditMode: false,
-        input: this.props.status
-    }
+		state = {
+				isEditMode: false,
+				input: this.props.status
+		}
 
-    toggleMode() {
-        this.setState({
-            isEditMode: !this.state.isEditMode,
-        })
-    }
+		toggleMode() {
+				this.setState({
+						isEditMode: !this.state.isEditMode,
+				})
+		}
 
-    changeInput (e: ChangeEvent<HTMLInputElement>) {
-        this.setState({
-            input: e.currentTarget.value
-        })
-    }
+		changeInput(e: ChangeEvent<HTMLInputElement>) {
+				this.setState({
+						input: e.currentTarget.value
+				})
+		}
 
-    componentDidUpdate(prevProps: Readonly<StatusPropsType>, prevState: Readonly<any>,) {
+		componentDidUpdate(prevProps: Readonly<StatusPropsType>, prevState: Readonly<any>,) {
 
-        if (this.props.status !== prevProps.status)
-        this.setState({
-            input: this.props.status
-        })
-    }
+				if (this.props.status !== prevProps.status)
+						this.setState({
+								input: this.props.status
+						})
+		}
 
 
-    render() {
-        const handlerClickButtonStatus = () => {
-            if (this.state.isEditMode) {
-                this.props.updateStatus(this.state.input)
-                toggleModeCB()
-            }
-            else {
-                toggleModeCB()
-            }
-        }
-        const toggleModeCB = this.toggleMode.bind(this)
-        const onChangeInputCB = this.changeInput.bind(this)
-        const viewStatus = <span onDoubleClick={toggleModeCB}>{this.state.input}</span>
-        const changeStatus =
-            <input
-                onChange={onChangeInputCB}
-                // onBlur={toggleModeCB}
-                type={'text'}
-                value={this.state.input}
-                autoFocus
-            />
-        const renderStatus = this.state.isEditMode ? changeStatus : viewStatus
-        return (
-            <div className={cl.status}>
-                {renderStatus}
-                {this.props.isHostUser &&
-                    <button onClick={handlerClickButtonStatus}>{this.state.isEditMode ? 'Save' : 'Edit'}</button>
-                }
-            </div>
-        )
-    }
+		render() {
+				const handlerClickButtonStatus = () => {
+						if (this.state.isEditMode) {
+								this.props.updateStatus(this.state.input)
+								toggleModeCB()
+						} else {
+								toggleModeCB()
+						}
+				}
+				const toggleModeCB = this.toggleMode.bind(this)
+				const onChangeInputCB = this.changeInput.bind(this)
+				const viewStatus = <span onDoubleClick={toggleModeCB}>{this.state.input}</span>
+				const changeStatus =
+						<input
+								onChange={onChangeInputCB}
+								// onBlur={toggleModeCB}
+								type={'text'}
+								value={this.state.input}
+								autoFocus
+						/>
+				const renderStatus = this.state.isEditMode ? changeStatus : viewStatus
+				return (
+						<div className={cl.status}>
+								{renderStatus}
+								{this.props.isHostUser &&
+								this.state.isEditMode ?
+										<SaveIcon className={cl.statusIcon}
+															onClick={handlerClickButtonStatus}/>
+										:
+										<EditIcon
+												className={cl.statusIcon}
+												onClick={handlerClickButtonStatus}/>
+								}
+						</div>
+				)
+		}
 }
